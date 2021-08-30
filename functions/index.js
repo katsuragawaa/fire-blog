58a8c0038369b804c5f5e3aff4d55c939e8130c9
Katsuragawa <andre.katsuragawa@gmail.com>
2021-08-29 21:52:36 -0300

    add firebase functions

---
+++ functions/index.js
@@ -0,0 +1,18 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+admin.initializeApp();
+
+exports.addAdminRole = functions.https.onCall(async (data) => {
+  try {
+		const user = await admin
+			.auth()
+			.getUserByEmail(data.email);
+		await admin.auth().setCustomUserClaims(user.uid, {
+			admin: true,
+		});
+		return { message: `Success! ${data.email} has been made an admin` };
+	} catch (err) {
+		console.log(err);
+	}
+});
